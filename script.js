55c7b6b7b1a9ffe363ff576fc901736035ac7a00
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-03-18 11:55:48 +0530

    Add styling to the list

--- script.js
+++ script.js
@@ -23,17 +23,21 @@ createList();
 
 // Inject list items into DOM
 function createList() {
-  [...richPeople].forEach((person, index) => {
-    const listItem = document.createElement("li");
-    listItem.setAttribute("data-index", index);
-    listItem.innerHTML = `
+  [...richPeople]
+    .map(a => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(a => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
+      listItem.innerHTML = `
     <span class="number">${index + 1}</span>
     <div class="draggable" draggable="true">
     <p class="person-name">${person}</p>
-    <i class="fas fa-grip-line"></i>
+    <i class="fas fa-grip-vertical"></i>
     </div>
     `;
-    listItems.push(listItem);
-    dragList.appendChild(listItem);
-  });
+      listItems.push(listItem);
+      dragList.appendChild(listItem);
+    });
 }
